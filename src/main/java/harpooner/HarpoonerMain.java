d6265ffab87d3955fc9b75432491f3a58c6337d5
Fernando Rojas <fernando.rojas@firstfactory.com>
2020-03-20 08:01:11 -0600

    first version

--- src/main/java/harpooner/HarpoonerMain.java
+++ src/main/java/harpooner/HarpoonerMain.java
@@ -9,6 +9,7 @@ import org.dreambot.api.wrappers.interactive.GameObject;
 import org.dreambot.api.wrappers.interactive.NPC;
 
 import java.awt.*;
+import java.util.Arrays;
 
 /**
  * Created by Computor on 7/8/2015.
@@ -17,6 +18,8 @@ import java.awt.*;
 public class HarpoonerMain extends AbstractScript {
 
     Area pierArea = new Area(2925, 3180, 2924, 3175, 0);
+    Area boatIslandArea =  new Area(2953, 3147, 2955, 3147, 0);
+    Area boatMainlandArea =  new Area(3029, 3220, 3026, 3215, 0);
 
     @Override
     public void onStart(){
@@ -28,9 +31,11 @@ public class HarpoonerMain extends AbstractScript {
         /**
          * Chopping trees: Time to chop some trees, our inventory isn't full. We want to fill it up.
          */
+        //testCode();
+
         if(!getInventory().isFull()){
             if(pierArea.contains(getLocalPlayer())){
-                chopTree("Tree"); //change "Tree" to the name of your tree.
+                getMeSomeFish("Fishing spot"); //change "Tree" to the name of your tree.
             }else{
                 if(getWalking().walk(pierArea.getRandomTile())){
                     sleep(Calculations.random(3000, 5500));
@@ -42,16 +47,46 @@ public class HarpoonerMain extends AbstractScript {
          * Banking: Time to bank our logs. Our inventory is full, we want to empty it.
          */
         if(getInventory().isFull()){ //it is time to bank
-            if(bankArea.contains(getLocalPlayer())){
-                bank();
+            if(boatIslandArea.contains(getLocalPlayer())){
+                goToMainland();
+                depositLoot();
+                goToKaramja();
             }else{
-                if(getWalking().walk(bankArea.getRandomTile())){
+                if(getWalking().walk(boatIslandArea.getRandomTile())){
                     sleep(Calculations.random(3000, 6000));
                 }
             }
+
+
+
         }
 
         return 600;
+    }
+
+    private void goToMainland() {
+        NPC boatGuy = getNpcs().closest(
+                n -> n != null && Arrays.toString(n.getActions()).contains("Pay-fare"));
+        boatGuy.interact("Pay-Fare");
+        sleep(Calculations.random(3000, 6000));
+        GameObject plank = getGameObjects().closest(n -> n != null && n.getID() == 2084);
+        plank.interact();
+    }
+
+    private void goToKaramja() {
+        while(! boatMainlandArea.contains(getLocalPlayer())){
+            if(getWalking().walk(boatMainlandArea.getRandomTile())){
+                randomCameraMovement();
+                sleep(Calculations.random(3000, 6000));
+            }
+        }
+        NPC boatGuy = getNpcs().closest(
+                n -> n != null && Arrays.toString(n.getActions()).contains("Pay-fare"));
+        boatGuy.interact("Pay-Fare");
+        sleep(Calculations.random(5000, 7000));
+        GameObject plank = getGameObjects().closest(n -> n != null && n.getID() == 2082);
+        plank.interact();
+
     }
 
     @Override
@@ -65,10 +100,21 @@ public class HarpoonerMain extends AbstractScript {
     }
 
     private void getMeSomeFish(String nameOfTile){
-        GameObject fishingSpot = getGameObjects().closest(gameObject -> gameObject != null && gameObject.getName().equals(nameOfTile));
+        log("Trying to get some fish.");
+        NPC fishingSpot = getNpcs().closest(
+                n -> n != null && Arrays.toString(n.getActions()).contains("Harpoon") && Arrays.toString(n.getActions()).contains("Cage"));
+
+
         if(fishingSpot != null && fishingSpot.interact("Harpoon")){
-            int countFish = getInventory().count("Raw Swordfish") + getInventory().count("Raw tuna");
-            sleepUntil(() -> getInventory().count("Logs") > countLog, 12000);
+            log("Found fishing spot: " + fishingSpot.getName() + " Coordinates: "+ fishingSpot.getGridX() + " - " + fishingSpot.getGridY());
+            sleepUntil(() -> getLocalPlayer().isStandingStill(), Calculations.random(14000, 31000));
+            randomCameraMovement();
+        }
+    }
+
+    private void randomCameraMovement() {
+        if(Calculations.random(0, 9) > 4){
+            getCamera().rotateTo(Calculations.random(2400), Calculations.random(getClient().getLowestPitch(), 384));
         }
     }
 
@@ -85,5 +131,19 @@ public class HarpoonerMain extends AbstractScript {
                 }
             }
         }
+    }
+
+    private void testCode(){
+        goToKaramja();
+    }
+
+    private void depositLoot(){
+        randomCameraMovement();
+        getDepositBox().open();
+        if(getDepositBox().isOpen()){
+            getDepositBox().depositAll("Raw tuna");
+            getDepositBox().depositAll("Raw swordfish");
+        }
+        getDepositBox().close();
     }
 }
